d3278368564f2d8764e6a702a1e1e7adc5b94698
Wenke-D <matthieu.du@outlook.com>
2022-04-13 22:44:03 +0200

    finish seq matrix multiplication

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,13 +1,23 @@
 #include <stdexcept>
 #include <vector>
+#include <string>
 
 template <typename T> class Matrix {
   private:
     std::vector<T> data;
+
+
+  public:
     size_t width;
     size_t height;
 
-  public:
+    /**
+     * @brief Construct a new Matrix object by declearing its size and default value
+     * 
+     * @param width width of the matrix
+     * @param height height of the matrix
+     * @param default_value default value of all elements
+     */
     Matrix(size_t width, size_t height, T default_value)
         : width(width), height(height) {
         data.resize(width * height);
@@ -21,8 +31,74 @@ template <typename T> class Matrix {
      */
     std::vector<T> operator[](size_t i);
 
+    /**
+     * @brief Get a the column j
+     * 
+     * @param j index of a col
+     * @return std::vector<T> 
+     */
+    std::vector<T> col(size_t j);
+
     void set(size_t i, size_t j, T v);
 
     template <typename U>
     friend std::ostream &operator<<(std::ostream &os, const Matrix<U> &m);
 };
+
+
+template <typename T>
+std::vector<T> Matrix<T>::operator[](size_t i) {
+    if (i >= height)
+        throw std::invalid_argument("Line too big");
+        
+    auto begin = this->data.begin() + i * this->width;
+    auto end = begin + this->width;
+    std::vector<T> line(begin, end);
+    return line;
+}
+
+template <typename T>
+std::vector<T> Matrix<T>::col(size_t j) {
+    if (j >= width)
+        throw std::invalid_argument("col too big");
+        
+    std::vector<T> col;
+    for (size_t i = j; i < this->data.size(); i+=this->width)
+    {
+        col.push_back(this->data[i]);
+    }
+    
+    return col;
+}
+
+/**
+ * @brief Set a value in location (i, j) of the matrix
+ * 
+ * @tparam T value type of martix
+ * @param i row index
+ * @param j col index
+ * @param v value
+ */
+template <typename T> void Matrix<T>::set(size_t i, size_t j, T v) {
+    if(i >= this->height || j >= this->width){
+        throw std::invalid_argument("Oversize");
+    }
+
+    this->data[i * width + j] = v;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const Matrix<T> &m) {
+    os << "Matrix" << std::endl;
+    for (size_t i = 0; i < m.data.size(); i++) {
+        os << m.data[i];
+        if ((i + 1) % m.width == 0) {
+            os << std::string("\n");
+        } else {
+            os << std::string(" ");
+        }
+    }
+    os << "----" << std::endl;
+
+    return os;
+}
--- main.cpp
+++ main.cpp
@@ -11,10 +11,40 @@
 #include <hpx/iostream.hpp>
 #include <hpx/numeric.hpp>
 
-#include "matrix.hpp"
-#include "output.hpp"
+#include "include/matrix.hpp"
+#include "include/output.hpp"
 
 using namespace std;
+
+/**
+ * @brief inner product of 2 vectors, assuming they have the same size.
+ *
+ * @param v1
+ * @param v2
+ * @return int
+ */
+int operator*(vector<int> v1, vector<int> v2) {
+    return hpx::transform_reduce(v1.begin(), v1.end(), v2.begin(), 0,
+                                 std::plus<int>(), std::multiplies<int>());
+}
+
+Matrix<int> operator*(Matrix<int> m1, Matrix<int> m2) {
+
+    size_t height = m1.height, width = m2.width;
+
+    Matrix<int> res{height, width, 0};
+
+    for (size_t i = 0; i < height; i++)
+    {
+        for (size_t j = 0; j < width; j++)
+        {   
+            int value = m1[i] * m2.col(j);
+            res.set(i, j, value);
+        }
+        
+    }
+    return res;
+}
 
 int main() {
 
@@ -22,7 +52,18 @@ int main() {
 
     Matrix<int> I_2{2, 2, 1};
 
-    cout << I_2 << endl;
+    cout << m1;
+    m1.set(0, 0, 5);
+    cout << m1;
+    m1.set(1, 1, 10);
+    cout << m1;
 
-    cout << I_2[0] << endl;
+    cout << m1[0] << endl;
+    cout << m1[1] << endl;
+    cout << m1.col(0) << endl;
+    cout << m1.col(1) << endl;
+
+    cout << m1[0] * m1[0] << endl;
+
+    cout << m1 * m1;
 }
